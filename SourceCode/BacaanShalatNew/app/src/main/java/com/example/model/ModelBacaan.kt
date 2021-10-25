9dd81a9f910c24bc2075fb57dbf21c83b7d13383
tyonesatriaaa <iostudio92@gmail.com>
2021-10-25 08:39:17 +0700

    update project

---
+++ SourceCode/BacaanShalatNew/app/src/main/java/com/example/model/ModelBacaan.kt
@@ -0,0 +1,10 @@
+package com.example.bacaanshalatnew.model
+
+
+class ModelBacaan {
+    var id: String? = null
+    var name: String? = null
+    var arabic: String? = null
+    var latin: String? = null
+    var terjemahan: String? = null
+}
