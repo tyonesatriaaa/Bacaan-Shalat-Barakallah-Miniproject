43ff300e9f32e0b276bb9abb332394d96a15cede
thaliaamiraa <thalia02amira@gmail.com>
2021-11-01 00:38:32 +0700

    Update Bacaan Shalat

---
+++ BacaanShalat/app/src/main/java/com/kuliah/bacaanshalat/TasyahudAwal.java
@@ -0,0 +1,14 @@
+package com.kuliah.bacaanshalat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TasyahudAwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasyahud_awal);
+    }
+}
